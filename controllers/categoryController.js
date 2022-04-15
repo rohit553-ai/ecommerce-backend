e65256953f1e67341c778d90963759a2705f69b0
Prakash Pun <poonprakash22@gmail.com>
2022-04-15 08:19:11 +0545

    update message

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,60 +1,59 @@
-const {categoryService} = require("../services");
+const { categoryService } = require("../services");
 
 let categoryController = {};
 
-categoryController.getAllCategories = async(req, res, next)=>{
+categoryController.getAllCategories = async (req, res, next) => {
   const categories = await categoryService.findAll();
   return res.status(200).json(categories);
-}
+};
 
-categoryController.getSingleCategories = async(req, res, next)=>{
+categoryController.getSingleCategories = async (req, res, next) => {
   const category = await categoryService.findOne({
-    id: req.params.id
+    id: req.params.id,
   });
-  if(!category){
-    return next(new Error("Soemthing went wrong"));
+  if (!category) {
+    return next(new Error("Category not found!"));
   }
 
   return res.status(200).json(category);
-}
+};
 
-categoryController.postCategories = async(req, res, next)=>{
-  const {name} =req.body;
-  const newCategory = await categoryService.add({name});
+categoryController.postCategories = async (req, res, next) => {
+  const { name } = req.body;
+  const newCategory = await categoryService.add({ name });
 
   return res.status(200).json(newCategory);
-}
+};
 
-categoryController.updateCategory = async(req, res, next)=>{
+categoryController.updateCategory = async (req, res, next) => {
   let category = await categoryService.findOne({
-    id: req.params.id
-  })
+    id: req.params.id,
+  });
 
-  if(!category){
-    return next(new Error("Soemthing went wrong"));
+  if (!category) {
+    return next(new Error("Something went wrong!"));
   }
 
-  if(req.body.name){
+  if (req.body.name) {
     category.name = req.body.name;
   }
   category = await category.save();
   return res.status(200).json(category);
-}
+};
 
-categoryController.deleteCategory = async(req, res, next)=>{
+categoryController.deleteCategory = async (req, res, next) => {
   const category = await categoryService.findOne({
-    id: req.params.id
-  })
-
-  if(!category){
-    return next(new Error("Soemthing went wrong"));
-  }
-  await categoryService.delete({id: category.id});
-  return res.status(200).json({
-    status:"success",
-    data: null
+    id: req.params.id,
   });
-}
+
+  if (!category) {
+    return next(new Error("Something went wrong!"));
+  }
+  await categoryService.delete({ id: category.id });
+  return res.status(200).json({
+    status: "success",
+    data: null,
+  });
+};
 
 module.exports = categoryController;
-
--- controllers/productController.js
+++ controllers/productController.js
@@ -1,32 +1,32 @@
-const {productService, categoryService} = require("../services");
-const {CustomError} = require("../helpers")
+const { productService, categoryService } = require("../services");
+const { CustomError } = require("../helpers");
 
 let productController = {};
 
-productController.getProducts = async(req, res, next)=>{
+productController.getProducts = async (req, res, next) => {
   const filterQuery = productService.buildQuery(req);
 
   console.log(filterQuery);
 
   const data = await productService.findAll(filterQuery);
   return res.status(200).json(data);
-}
+};
 
-productController.getSingleProduct = async(req, res, next)=>{
+productController.getSingleProduct = async (req, res, next) => {
   const product = await productService.findOne({
-    id: req.params.id
-  })
+    id: req.params.id,
+  });
 
-  if(!product){
-    return next(new CustomError("Can't find product with given id", 404))
+  if (!product) {
+    return next(new CustomError("Can't find product with given id", 404));
   }
 
   return res.status(200).json(product);
-}
+};
 
-productController.addNewProduct = async(req, res, next)=>{
-  let category = await categoryService.findOne({id: req.body.categoryId});
-  if(!category){
+productController.addNewProduct = async (req, res, next) => {
+  let category = await categoryService.findOne({ id: req.body.categoryId });
+  if (!category) {
     return next(new CustomError("Invalid category for prodcut", 400));
   }
 
@@ -35,48 +35,48 @@ productController.addNewProduct = async(req, res, next)=>{
     categoryId: req.body.categoryId,
     description: req.body.description,
     quantity: req.body.quantity,
-    price: req.body.price
-  }
+    price: req.body.price,
+  };
 
   const newProduct = await productService.add(productData);
   return res.status(200).json(newProduct);
-}
+};
 
-productController.updateProduct = async(req, res, next)=>{
-  let {name, description, quantity, price} = req.body;
+productController.updateProduct = async (req, res, next) => {
+  let { name, description, quantity, price } = req.body;
 
   let product = await productService.findOne({
-    id: req.params.id
-  })
+    id: req.params.id,
+  });
 
-  if(!product){
-    return next(new CustomError("Can't find product with given id", 404))
+  if (!product) {
+    return next(new CustomError("Can't find product with given id", 404));
   }
 
-  name? product.name = name : null;
-  description? product.description = description : null;
-  quantity? product.quantity = quantity : null;
-  price? product.price = price : null;
+  name ? (product.name = name) : null;
+  description ? (product.description = description) : null;
+  quantity ? (product.quantity = quantity) : null;
+  price ? (product.price = price) : null;
 
   product = await product.save();
 
   return res.status(200).json(product);
-}
+};
 
-productController.deleteProduct = async(req, res, next)=>{
+productController.deleteProduct = async (req, res, next) => {
   let product = await productService.findOne({
-    id: req.params.id
-  })
+    id: req.params.id,
+  });
 
-  if(!product){
-    return next(new CustomError("Can't find product with given id", 404))
+  if (!product) {
+    return next(new CustomError("Can't find product with given id", 404));
   }
 
-  await productService.delete({id: req.params.id});
+  await productService.delete({ id: req.params.id });
   return res.status(200).json({
-    status:"success",
-    data: null
-  })
-}
+    status: "success",
+    data: null,
+  });
+};
 
 module.exports = productController;
