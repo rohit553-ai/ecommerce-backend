99f72aeca8d1ce8b32ca9629010f855a4a281c52
rohit553-ai <68155913+rohit553-ai@users.noreply.github.com>
2022-04-23 11:36:09 +0545

    removed middlware verify login

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ userRouter.get("/", verifyLogin, wrapAsync(userController.getAllUsers))
 userRouter.get("/me", verifyLogin, userController.me);
 userRouter.patch("/me", verifyLogin, profileUpdateValidator, checkErrors, wrapAsync(userController.updateProfile));
 userRouter.get("/messages", verifyLogin, wrapAsync(userController.getMessages));
-userRouter.post("/messages", verifyLogin, messageValidator, checkErrors, wrapAsync(userController.sendMessages));
+userRouter.post("/messages", messageValidator, checkErrors, wrapAsync(userController.sendMessages));
 userRouter.get("/my/orders", verifyLogin, wrapAsync(orderController.getMyOrders));
 userRouter.get("/my/wish-list", verifyLogin, wrapAsync(userController.getMyWishList));
 userRouter.delete("/my/wish-list/:id", verifyLogin, wrapAsync(userController.deleteMyWish));
